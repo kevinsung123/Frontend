729a64314d8fab0a17f941344fd11dbda1a1c4b0
Wonjae Kim <wonjaek36@gmail.com>
2018-05-09 18:42:51 +0900

    Header-menu: change css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,13 +5,13 @@ import {NavLink,Link} from 'react-router-dom'
 export class Header extends React.Component {
     render() {
         return (
-            <div id="header">
-                <div id="left-header">
+            <div class="page-header" >
+                <h1 id="header-left">
                     <NavLink to="/">MAIN</NavLink>
-                </div>
-                <div id="right-header">
-                    <h1>IMSAFE 중앙보훈병원</h1>
-                </div>
+                </h1>
+                <h2 id="header-right">
+                    IMSAFE 중앙보훈병원
+                </h2>
             </div>
         );
     }
