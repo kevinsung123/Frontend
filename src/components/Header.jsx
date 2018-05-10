1ad339ec99747213e84e79464cc24175d2f78f0b
Wonjae Kim <wonjaek36@gmail.com>
2018-05-10 21:19:23 +0900

    Header: add skelton code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,35 +1,140 @@
 import React from 'react';
 import {NavLink,Link} from 'react-router-dom'
-import AppBar from 'material-ui/AppBar';
-import FlatButton from 'material-ui/FlatButton';
-import Tabs from 'material-ui/Tabs';
-//import Tab from 'material-ui/Tab';
+
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
+import Popover from 'material-ui/Popover';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
+import RaisedButton from 'material-ui/RaisedButton';
+
 //import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 export class Header extends React.Component {
-
-    get titleStyle() {
-        return {
-            cursor: 'pointer',
-            color: 'black'
-        }
-    }
-
-    get flatButtonStyle() {
-        return {
-            backgroundColor: 'transparent',
-            color: 'white',
-        }
-    }
 
     titleClick() {
         {location.href = '/'}
     }
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            patientMenuOpen:false,
+            ruleMenuOpen: false,
+        };
+    }
+
+    clickPatientsMenuButton = (event) => {
+        event.preventDefault();
+
+        this.setState({
+            patientMenuOpen: true,
+            patientMenuAnchorEl: event.currentTarget,
+        });
+    };
+
+    clickRuleMenuButton = (event) => {
+        event.preventDefault();
+
+        this.setState({
+            ruleMenuOpen: true,
+            ruleMenuAnchorEl: event.currentTarget,
+        })
+    }
+
+    handlePaitentsMenuButtonClose = () => {
+        this.setState({
+            patientMenuOpen: false,
+        })
+    }
+
+    handleRuleMenuButtonClose = () => {
+        this.setState({
+            ruleMenuOpen: false,
+        })
+    }
+
     render() {
         return (
             <div class="container width-max-100">
-                <AppBar
+
+                <Toolbar>
+                    <ToolbarGroup>
+                        <ToolbarTitle text="Main" />
+                        <ToolbarSeparator />
+                        <div>
+                            <RaisedButton 
+                                onClick={this.clickPatientsMenuButton}
+                                label="환자 업무"
+                            />
+
+                            <Popover
+                                open={this.state.patientMenuOpen}
+                                anchorEl={this.state.patientMenuAnchorEl}
+                                anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
+                                targetOrigin={{horizontal: 'left', vertical: 'top'}}
+                                onRequestClose={this.handlePaitentsMenuButtonClose}
+                            >
+                                <Menu>
+                                    <Link expect to="/patients/add">
+                                        <MenuItem 
+                                            onClick={this.handlePaitentsMenuButtonClose}
+                                            primaryText="새로운 환자 추가" />
+                                    </Link>
+                                    <Link expect to="/patients/search">
+                                        <MenuItem 
+                                            onClick={this.handlePaitentsMenuButtonClose}
+                                            primaryText="환자 검색 및 정보 수정" />
+                                    </Link>
+                                </Menu>
+                            </Popover>
+                        </div>
+
+                        <div>
+                            <RaisedButton
+                                onClick={this.clickRuleMenuButton}
+                                label="규칙 업무"
+                            />
+
+                            <Popover
+                                open={this.state.ruleMenuOpen}
+                                anchorEl={this.state.ruleMenuAnchorEl}
+                                anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
+                                targetOrigin={{horizontal: 'left', vertical: 'top'}}
+                                onRequestClose={this.handleRuleMenuButtonClose}
+                            >
+                                <Menu>
+                                    <Link expect to="/rules/add">
+                                        <MenuItem 
+                                            onClick={this.handleRuleMenuButtonClose}
+                                            primaryText="새로운 규칙 추가" />
+                                    </Link>
+                                    <Link expect to="/rules/search">
+                                        <MenuItem 
+                                            onClick={this.handleRuleMenuButtonClose}
+                                            primaryText="규칙 검색 및 정보 수정" />
+                                    </Link>
+                                </Menu>
+                            </Popover>
+                        </div>
+
+                    </ToolbarGroup>
+                </Toolbar>
+
+               
+            </div>
+        );
+    }
+}
+
+
+/*<li><NavLink expect to="/patients/search" activeStyle={activeStyle}>검색</NavLink></li>
+<Route exact path="/" component={MainPage} />
+                    <Route exact path="/patients/search" component={PatientSearchPage}/>
+                    <Route exact path="/patients/add" component={PatientsDetail} />
+
+
+ <AppBar
                     title={
                         <Link expect to="/">
                         <span style={this.titleStyle}
@@ -47,11 +152,5 @@ export class Header extends React.Component {
                         <FlatButton label="규칙" style={this.flatButtonStyle} />
                     </NavLink>
                 </AppBar>
-            </div>
-        );
-    }
-}
-/*<li><NavLink expect to="/patients/search" activeStyle={activeStyle}>검색</NavLink></li>
-<Route exact path="/" component={MainPage} />
-                    <Route exact path="/patients/search" component={PatientSearchPage}/>
-                    <Route exact path="/patients/add" component={PatientsDetail} />*/
+
+                    */
