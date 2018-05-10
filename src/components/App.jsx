68fbe41e0f25ace2e3c48a8df6e244c15f6a1e07
SKH <kevinsung123@gmail.com>
2018-05-11 02:06:22 +0900

    pateint form & fix error

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import {BrowserRouter} from 'react-router-dom'
 import {Header} from './Header'
-import {SideMenu} from './SideMenu'
+
 import {Content} from './Content'
 
 class App extends React.Component {
@@ -10,12 +10,7 @@ class App extends React.Component {
 
         <div id="main_wrapper" >
           <Header />
-          <div id="side_menu">
-             {/* <SideMenu />  */}
-          </div> 
-          <div>
           <Content />
-          </div>
         </div>
       
       );
